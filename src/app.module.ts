f1ab1036b235fc10172a1b6cf6238b8d03c365bb
willy <willybalt.2017@gmail.com>
2025-07-16 15:15:22 -0400

    conexion con mongo atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule, 
+    AuthModule,
+    MongooseModule.forRoot('mongodb+srv://admin:1234567890@cluster0.vgbrok7.mongodb.net/users?retryWrites=true&w=majority&appName=Cluster0')
+  ],
   controllers: [AppController],
   providers: [AppService],
 }) 
 export class AppModule {}
+
