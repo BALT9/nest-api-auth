0a97efeb031c098f8da0ebaddad611d0861aa09c
willy <willybalt.2017@gmail.com>
2025-07-16 16:25:58 -0400

    se agrego el schema y los dto

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    
+
+    constructor(private authService: AuthService){}
+
+    @Post()
+    funlogin(@Body() datos: Record<string, any>){
+        return this.authService.login(datos.email, datos.password);
+    }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -2,8 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+
 @Injectable()
 export class UsersService {
+
+  // importamos el modelo 
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>
+  ){}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -12,15 +22,15 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  update(id: string, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} user`;
   }
 }
