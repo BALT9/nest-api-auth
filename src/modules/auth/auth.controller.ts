d58c114f7e9b388f32e38043d33a15a4a0df5fc9
willy <willybalt.2017@gmail.com>
2025-07-17 09:24:40 -0400

    autenticacion y rutas protegidas con jwt

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,11 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, UseGuards, Request, NotFoundException } from '@nestjs/common';
 import { AuthService } from './auth.service';
-// dto 
 import { CreateUserDto } from '../users/dto/create-user.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt/jwt.guard';
+import { UsersService } from '../users/users.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService
+  ) {}
 
   @Post('login')
   login(@Body() datos: { email: string; password: string }) {
@@ -15,5 +19,18 @@ export class AuthController {
   @Post('register')
   register(@Body() userData: CreateUserDto) {
     return this.authService.register(userData);
+  }
+
+  // 👉 NUEVA RUTA DE PERFIL
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  async getProfile(@Request() req) {
+    const user = await this.usersService.findOne(req.user.userId); // "sub" en el token
+    if (!user) {
+      throw new NotFoundException('Usuario no encontrado');
+    }
+
+    const { password, ...userWithoutPassword } = user.toObject();
+    return userWithoutPassword;
   }
 }
